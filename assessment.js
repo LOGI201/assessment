fde1dd7a6324ce93205e0df8be815f5bf56e92a8
LOGI201 <yuuta_24s1100045@nnn.ed.jp>
2025-03-23 23:51:47 +0900

    Bootstrapに対応

--- assessment.js
+++ assessment.js
@@ -15,14 +15,28 @@ assessmentButton.addEventListener(
 
     // 診断結果表示エリアの作成
     resultDivision.innerText= '';
-    const heading = document.createElement('h3');
-    heading.innerText = '診断結果';
-    resultDivision.appendChild(heading);
+
+    // headerDivision の作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class','card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    // bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class','card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class','card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    // resultDivision に Bootstrap のスタイルを適用する
+    resultDivision.setAttribute('class','card');
+
+    // headerDivision と bodyDivision を resultDivision に差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
 
     // ツイートエリアの作成
     tweetDivision.innerText = '';
@@ -33,7 +47,7 @@ assessmentButton.addEventListener(
     anchor.setAttribute('href',hrefValue);
     anchor.setAttribute('class','twitter-hashtag-button');
     anchor.setAttribute('data-text',result);
-    anchor.innerText = 'Tweet#あなたのいいところ';
+    anchor.innerText = 'Tweet #あなたのいいところ';
 
     tweetDivision.appendChild(anchor);
 
@@ -47,7 +61,7 @@ assessmentButton.addEventListener(
 userNameInput.addEventListener(
   'keydown',
   (event) => {
-    if(event.code === 'Enter') {
+    if (event.code === 'Enter') {
       assessmentButton.dispatchEvent(new Event('click'));
     }
   }
